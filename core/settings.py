8a03a68448e1d9164d200026b317ff2234608585
winsobusinessservices <winsobusiness24@gmail.com>
2026-09-12 13:15:02 +0530

    Refactor EMAIL_USE_TLS assignment for clarity

--- core/settings.py
+++ core/settings.py
@@ -466,7 +466,9 @@ EMAIL_PORT = int(
     os.getenv("EMAIL_PORT", "587")
 )
 
-os.getenv("EMAIL_USE_TLS", "True") == "True"
+EMAIL_USE_TLS = (
+    os.getenv("EMAIL_USE_TLS", "True") == "True"
+)
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 
